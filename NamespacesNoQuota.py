03db5cc69e5775c35a5a1e614b6a32c45e46c9a2
Nuland, Chris <Chris.Nuland@anthem.com>
2020-05-19 15:54:56 -0400

    Allow script to work on windows

--- NamespacesNoQuota.py
+++ NamespacesNoQuota.py
@@ -1,8 +1,19 @@
-import commands
-oc=commands.getstatusoutput('oc get namespaces -o name | cut -c 11-')
-namespaces = oc[1].split("\n")
+import subprocess
+
+def bash_command_pipe(cmd): 
+    ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    return ps.communicate()[0]
+
+def bash_command(cmd): 
+    return subprocess.check_output(cmd)
+
+output = bash_command_pipe("oc get namespaces -o name | cut -c 11-")
+namespaces = output.split("\n")
 for ns in namespaces:
     if not ns.startswith("openshift") and not ns.startswith("kube") and not ns.startswith("default"):
-        oc=commands.getstatusoutput('oc get quota  --ignore-not-found=true --namespace {}'.format(ns))
-        if not oc[1]:
+        if not ns:
+            continue
+        cmd = 'oc get quota --ignore-not-found=true --namespace {}'.format(ns)
+        oc = bash_command(cmd)
+        if not oc:
             print(ns)
